9a0fb9fc511318455ddf50762b5e552cad671a4f
Laurent Vaylet <lvaylet@talend.com>
2017-09-14 11:06:55 +0200

    add ssl on top of basic auth

--- cookbooks/nginx_ssl_auth/recipes/default.rb
+++ cookbooks/nginx_ssl_auth/recipes/default.rb
@@ -1,13 +1,24 @@
 include_recipe 'chef_nginx'
 
-# FIXME How to handle keyboard input for the certificate settings?
-# https://superuser.com/questions/226192/avoid-password-prompt-for-keys-and-prompts-for-dn-information/1086972
-#execute 'generate_certs' do
-#  # Is 'sudo' required? Does chef-solo have root access?
-#  command 'sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./cert.key -out ./cert.crt'
-#  command 'sudo openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.example.com" -keyout www.example.com.key -out www.example.com.cert'
-#  cwd '/etc/nginx'
-#end
+# Setup self-signed certificates in /etc/nginx
+#
+# Use headless mode, equivalent to typing the following in interactive mode:
+#   Country Name (2 letter code) [AU]:FR
+#   State or Province Name (full name) [Some-State]:IDF
+#   Locality Name (eg, city) []:Paris
+#   Organization Name (eg, company) [Internet Widgits Pty Ltd]:ACME
+#   Organizational Unit Name (eg, section) []:
+#   Common Name (eg, YOUR name) []:192.168.0.54
+#   Email Address []:
+#
+# References:
+#   - https://www.digitalocean.com/community/tutorials/how-to-create-a-self-signed-ssl-certificate-for-nginx-in-ubuntu-16-04
+#   - https://superuser.com/questions/226192/avoid-password-prompt-for-keys-and-prompts-for-dn-information/1086972
+execute 'generate_certs' do
+  # Is 'sudo' required? Does chef-solo have root access?
+  command 'sudo openssl req -x509 -nodes -days 365 -newkey rsa:4096 -new -subj "/C=US/ST=IDF/L=Paris/O=ACME/CN=192.168.50.4" -keyout ./cert.key -out ./cert.crt'
+  cwd '/etc/nginx'
+end
 
 # Configure HTTP basic authentication
 # https://www.digitalocean.com/community/tutorials/how-to-set-up-password-authentication-with-nginx-on-ubuntu-14-04
@@ -18,7 +29,8 @@ end
 
 # Render /etc/nginx/sites-available/default
 template '/etc/nginx/sites-available/default' do
-  source 'default.basic_auth.erb'
+#  source 'default.basic_auth.erb'
+  source 'default.ssl_basic_auth.erb'
 end
 
 # Render /usr/share/nginx/html/index.html (the default location served by Apache)
