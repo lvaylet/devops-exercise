82fe6343a1e226a4a56daa90e37ed70de2379e2c
Laurent Vaylet <lvaylet@talend.com>
2017-09-14 16:44:32 +0200

    add doc

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,7 @@
 name             'devopsexercise'
 maintainer       'Laurent Vaylet'
 maintainer_email 'laurent.vaylet@gmail.com'
-description      'Provision a static server and Nginx'
-version          '0.1.0'
+description      'Provision a static server and use Nginx as reverse proxy over SSL with basic auth'
+version          '1.0.0'
 
 depends 'nodejs'
-#depends 'chef_nginx'
